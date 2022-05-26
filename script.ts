a37b24162af230660187a40da9cf05ad098dd750
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-26 16:44:21 -0700

    compiled ts to js

--- script.ts
+++ script.ts
@@ -1,22 +1,37 @@
 // const greeting: string = "Typescripts test";
-// document.getElementById("type-s").innerHTML = greeting;
+document.getElementById("type-s").innerHTML = "greeting";
 // console.log(greeting);
 
 // tsc script
 // ts-node script
-function discount_calculation(
-  original_price: number,
-  discount_percentage: number
-): void {
+// document.getElementById("new_price").innerHTML = "";
+// original_price: number,
+//   discount_percentage: number
+
+// original_price = parseFloat(document.getElementById("original_price").value);
+
+// console.log(original_price);
+
+function discount_calculation(): void {
+  let original_price: number = parseFloat(
+    document.getElementById("original_price").value
+  );
+  console.log(original_price);
+  let discount_percentage = document.getElementById(
+    "discount_percentage"
+  ).value;
+  console.log(discount_percentage);
+
   //   console.log(original_price);
   let decimal: number = (100 - discount_percentage) / 100;
   let you_save: number = original_price * (discount_percentage / 100);
   let new_price: number = original_price * decimal;
-  console.log("New Price: $" + new_price);
-  console.log("You Save: $" + you_save);
+  //   console.log("New Price: $" + new_price);
+  //   console.log("You Save: $" + you_save);
+  document.getElementById("new_price").innerHTML = new_price;
+  //   document.getElementById("monthly_payments").innerHTML = you_save;
 }
 
-// pic with arrows like loan sharks
-let original_price: number = 70;
-let discount_percentage: number = 20;
-discount_calculation(original_price, discount_percentage);
+// let original_price: number = 70;
+// let discount_percentage: number = 20;
+// discount_calculation(original_price, discount_percentage);
